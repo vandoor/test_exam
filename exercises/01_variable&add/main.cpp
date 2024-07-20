10f6b318bf7a36c4e0c48c0f968b4c85044985ef
vandoor <1506127307@qq.com>
2024-07-21 00:06:03 +0800

    test

--- exercises/01_variable&add/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "../exercise.h"
-
-int main(int argc, char **argv) {
-    // TODO: 补全变量定义并打印加法运算
-    int x=1;
-    std::cout << x << " + " << x << " = " << x + x << std::endl;
-    return 0;
-}
